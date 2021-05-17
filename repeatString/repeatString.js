76432a8b364dd5547a888018eaf911d69a3df593
jesper <jesper@staahlarkitektur.no>
2021-05-17 12:48:06 +0200

    Complete repeatString exercises with some help

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(text,repeats) {
+    if (repeats < 0) return 'ERROR'
+    let string = ''
+    for (i = 0; i < repeats; i++) { 
+        string +=text;
+      }
+    return string;
 }
-
 module.exports = repeatString
