9fd03b52760710b241127b0344f3b1dc56ed6742
jesper <jesper@staahlarkitektur.no>
2021-05-17 12:59:39 +0200

    Complete reverseString no cap

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverseWord = word.split('');
+    let newWord= reverseWord.reverse();
+    return newWord.join('');
 }
 
 module.exports = reverseString
