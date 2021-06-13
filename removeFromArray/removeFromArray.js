d20807fdedcf4d76c4376087afa307e5d69ef684
jesper <jesper@staahlarkitektur.no>
2021-06-13 21:58:11 +0200

    complete removeFromArray with massive googling

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
 
-}
-
+const removeFromArray = function (...args) { //I had no idea that ...args was a thing. It takes unspecified amounts of arguments.
+   const array = args[0]; //the first argument is always the array
+   const newArray = [];
+   array.forEach((item) => {
+     if (!args.includes(item)) {
+       newArray.push(item);
+     }
+   });
+   return newArray;
+ };
 module.exports = removeFromArray
