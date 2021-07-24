074423b04ee88374e0d2c994ce6f9d8c10eb38f1
Jesper Staahl <jesper@staahlarkitektur.no>
2021-07-24 23:04:15 +0200

    need to solve for non-integer test

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,15 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let arr = [a,b];
     let fullArr = [];
     let sum = 0;
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
     arr.sort(function (a,b){return a-b});
 
-    for (let i = arr[0]; i <= arr[1]M i++){
+    for (let i = arr[0]; i <= arr[1]; i++){
         fullArr.push(i);
     }
     sum = fullArr.reduce(reducer);
     return sum;
+
+    
 }
 
 module.exports = sumAll
