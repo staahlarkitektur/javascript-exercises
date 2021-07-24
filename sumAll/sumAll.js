ff2e9bce64663d1d63e44e0bb445c2e9a5a28f4b
Jesper Staahl <jesper@staahlarkitektur.no>
2021-07-24 21:50:40 +0200

    tutorial followed step

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,15 @@
 const sumAll = function() {
+    let fullArr = [];
+    let sum = 0;
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
+    arr.sort(function (a,b){return a-b});
+
+    for (let i = arr[0]; i <= arr[1]M i++){
+        fullArr.push(i);
+    }
+    sum = fullArr.reduce(reducer);
+    return sum;
 }
 
 module.exports = sumAll
