609c8103c394fae33bf30863fa1f03291dcf3611
Jesper Staahl <jesper@staahlarkitektur.no>
2021-07-25 14:30:17 +0200

    complete leanYears intuitively

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (Number.isInteger(year/400)) {
+        return true;
+    }
+    if (Number.isInteger(year/100)) {
+        return false;
+    }
+    if (Number.isInteger(year/4)) {
+        return true;
+    }
+    if (!Number.isInteger(year/4)) {
+        return false;
+    }
+
 
 }
 
